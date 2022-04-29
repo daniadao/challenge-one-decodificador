397279d9ac9a6442d0e65922f6bd20ae597ad5d7
Daniela Adão <daniacadam@gmail.com>
2022-04-28 21:53:42 -0300

    atualização html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function criptografar(){
-	var texto = document.getElementById("text").value.toLowerCase();
-	var criptado = texto.replace(/e/igm,"enter");
+	var mensagem = document.getElementById("text").value.toLowerCase();
+	var criptado = mensagem.replace(/e/igm,"enter");
 	var criptado = criptado.replace(/o/igm,"ober");
 	var criptado= criptado.replace(/i/igm,"imes");
 	var criptado = criptado.replace(/a/igm,"ai");
 	var criptado = criptado.replace(/u/igm,"ufat");
-	document.getElementById("imgquality").style.display = "none";
+	
 	document.getElementById("texto").style.display = "none";
 	document.getElementById("texto2").innerHTML = criptado;
 	document.getElementById("btn-copiar").style.display = "show";
@@ -13,13 +13,13 @@ function criptografar(){
 	
 }
 function descriptografar(){
-	var texto = document.getElementById("text").value.toLowerCase();
-	var criptado = texto.replace(/enter/igm,"e");
+	var mensagem = document.getElementById("text").value.toLowerCase();
+	var criptado = mensagem.replace(/enter/igm,"e");
 	var criptado = criptado.replace(/ober/igm,"o");
 	var criptado = criptado.replace(/imes/igm,"i");
 	var criptado = criptado.replace(/ai/igm,"a");
 	var criptado = criptado.replace(/ufat/igm,"u");
-	document.getElementById("imgquality").style.display = "none";
+	
 	document.getElementById("texto").style.display = "none";
 	document.getElementById("texto2").innerHTML = criptado;
 	document.getElementById("btn-copiar").style.display = "show";
